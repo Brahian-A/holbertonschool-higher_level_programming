fe7a637c654f4e4b44dbf5eddc7f9d670fcff126
Brahia-A <10114@holbertonstudents.com>
2025-01-17 19:13:48 -0300

    print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -3,6 +3,6 @@ import sys
 
 def print_message():
     
-    print("Programming is like building a multilingual puzzle")
+    print("\"Programming is like building a multilingual puzzle")
 
 print_message()
