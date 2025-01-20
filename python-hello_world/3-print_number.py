39d3bf1309acc51d653826cc38de6f5d25833003
root <10114@holbertonstudents.com>
2025-01-20 00:37:45 +0000

    ahora?

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-import sys
-
-def print_battery_street(number):
-    print(f"{number} Battery street")
-
-print_battery_street(int(sys.argv[1]))
+number = 98
+print(f'{number} Battery street')
