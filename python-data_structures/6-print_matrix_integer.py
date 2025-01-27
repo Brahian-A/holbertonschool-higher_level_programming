9e955b1b482e32c42ac33c31481db97f01babd22
Brahia-A <10114@holbertonstudents.com>
2025-01-26 23:09:37 -0300

    ejercicio 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    for filas in matrix:
+        print (filas)
 
-    print matrix
+    for columnas in filas: 
+        print (columnas)
