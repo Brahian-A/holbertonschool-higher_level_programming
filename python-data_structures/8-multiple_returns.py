4f5a40e95047f3d89244698337eee606d73b24e2
Brahia-A <10114@holbertonstudents.com>
2025-01-28 19:17:11 -0300

    ejercicio 8 longitud y primer caracter de una tupla

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if not sentence:
-        return (None)
+        return (0, None)
     longitud = len(sentence)
     primer_c = sentence[0]
     return (longitud, primer_c)
