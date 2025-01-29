cc51fc7fe207a46b032e81d2bf7f123379d31523
Brahia-A <10114@holbertonstudents.com>
2025-01-29 12:39:48 -0300

    ejercicio 1 definicion de una clase con el atributo size y manejo de errores

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """en este modulo cramos una clase con el atributo size """
-"""comparamos si es un entero o mayor a 0 con su manejo de errores"""
 
 
 class Square:
     def __init__(self, size=0):
-        """" declaramos el atributo size y manejamos errores"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
