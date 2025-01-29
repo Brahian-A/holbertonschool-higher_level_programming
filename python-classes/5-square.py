e319f8db496a0fee69443082126d0afbba3f9407
Brahia-A <10114@holbertonstudents.com>
2025-01-29 16:47:31 -0300

    ejercicio 5 crear un cuadrado

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" definimos la clase """
+
+
+class Square:
+    """ manejamos casos de error y el area del square"""
+    def __init__(self, size=0):
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+    
+    def my_print(self):
+        mul = self.__size
+        
+        for i in range(mul):
+            for j in range(mul):
+                print("#", end=" ")
+            print()
