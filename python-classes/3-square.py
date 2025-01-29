7813820f4491bc9bde96e4dc8b5d50aa0770ab62
Brahia-A <10114@holbertonstudents.com>
2025-01-29 14:17:43 -0300

    ejercicio 3 crear una clase con size y area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+""" definimos la clase """
+
 
 class Square:
+    """ manejamos casos de error y el area del square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         s = self.__size
         return (s * s)
