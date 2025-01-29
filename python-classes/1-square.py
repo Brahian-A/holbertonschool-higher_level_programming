1122c95249e92ab2350a0bc9639d5121125dc37a
Brahia-A <10114@holbertonstudents.com>
2025-01-29 11:41:33 -0300

    ejercicio 1 definicion de una clase con el atributo size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" en este modulo creamos una clase con el atributo size"""
+
+
+class Square:
+    """definicion de la clase con el atributo size"""
+    def __init__(self, size):
+        self.__sizeof__ = size
