e29b6eb44d83c2f388a5eccae48af130c20db059
Brahia-A <10114@holbertonstudents.com>
2025-02-02 21:48:09 -0300

    ejercicio 3 print and str

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.__height * 2) + (self.__width * 2))
-    
+
     def my_prrint(self):
         if (self.__height or self.__width) == 0:
             print()
