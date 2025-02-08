ce048abe4b0a2771a4fa1b8ebe5eb5fff6b5e9b7
Brahia-A <10114@holbertonstudents.com>
2025-02-07 22:39:16 -0300

    4-list_division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
-        resultado = 0 
+        resultado = 0
         try:
             resultado = my_list_1[i] / my_list_2[i]
         except (ZeroDivisionError):
