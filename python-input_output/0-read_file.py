5b508d4ead9947ee7fe160b99bcea86f31a89b1b
Brahia-A <10114@holbertonstudents.com>
2025-02-11 01:33:55 -0300

    documentacion

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ modulo que lee un archivo de texto"""
+
+
 def read_file(filename=""):
     """funcion que abre y printea el contenido de un archivo UTF8"""
     with open(filename) as contenido:
-        print(contenido.read())
-        print()
+        return (print(contenido.read()))
