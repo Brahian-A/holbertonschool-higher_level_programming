c8e84605ce6888d96b7b63d86eb1478f3c201677
Brahia-A <10114@holbertonstudents.com>
2025-02-12 08:46:40 -0300

    documentacion

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""modulo que devueve un json string"""
+
 
 import json
 
 def from_json_string(my_str):
+    """funcion que retorna json lods """
     return json.loads(my_str)
