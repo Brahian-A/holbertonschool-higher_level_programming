89ef64136cc170f19590dfe89aeb3703f167283d
Brahia-A <10114@holbertonstudents.com>
2025-02-12 09:08:19 -0300

    documentacion

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""  """
+""" modulo """
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ """
+    """ funcion """
     with open(filename, mode='w', encoding="utf-8")as contenido:
         contenido.write(json.dumps(my_obj))
-        
