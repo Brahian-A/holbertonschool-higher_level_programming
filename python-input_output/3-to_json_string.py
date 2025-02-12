ce88728d43515ede6e73f9a8bcc29c8c7cf2e412
Brahia-A <10114@holbertonstudents.com>
2025-02-12 09:04:04 -0300

    documentacion

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" modulo que importa json strings"""
 
 import json
 
+
 def to_json_string(my_obj):
+    """funcion que retorna un json string con dumps"""
     return json.dumps(my_obj)
