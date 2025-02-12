2f3143a73a7d99e370d8256b7f475f851b49d187
Brahia-A <10114@holbertonstudents.com>
2025-02-12 08:58:41 -0300

    documentacion

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""" modulo que escribe en un archivio"""
+
 
 def write_file(filename="", text=""):
+    """funcion que escribe en un archivo de texto y retorna el contenido"""
     with open(filename, 'w') as contenido:
         return contenido.write(text)
