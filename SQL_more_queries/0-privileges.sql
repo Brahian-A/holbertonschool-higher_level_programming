21ea36119d66184a03d8c45eca54f118b363e07c
Brahia-A <10114@holbertonstudents.com>
2025-03-12 14:55:03 -0300

    permisos del usuario 1 y 2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- lista todos los privilegios de los usuarios user_0d_1 y user_0d_2
+-- permisos del usuario 1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- permisos del usuario 2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
