323f8887d74ad59e1f437d7eb34e466649365bdc
Brahia-A <10114@holbertonstudents.com>
2025-03-12 16:46:32 -0300

    ejercicio 1 permisos

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- crea un usuario con todos lo permisos y una contraseña
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost IDENTIFIED' BY 'user_0d_1_pwd';
+
+-- asignarle todos los permisos 
+GRANT ALL PRIVILEGES ON "." TO 'user_0d_1'@'localhost'
+FLUSH PRIVILEGES;
