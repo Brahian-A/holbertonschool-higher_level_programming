a556df1e25ea77fc40c68992f6622a58a8235d79
Brahia-A <10114@holbertonstudents.com>
2025-03-12 14:10:30 -0300

    task 15 y 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- script que enumera todos los registros de la tabla Second_Table d
+-- script que enumera todos los registros de la tabla Second_Table
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name !=''
