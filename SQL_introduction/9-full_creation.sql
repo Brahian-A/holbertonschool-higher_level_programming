12dfa1b413010434f53f6faf04cdb5e065fc7a60
Brahia-A <10114@holbertonstudents.com>
2025-03-12 13:23:06 -0300

    crea una segunda tabla con varias filas

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- script que crea una segunda tabla de datos y agrega varias filas
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id int,
+    name VARCHAR(256),
+    score int
+);
+
+-- inseta las nuevas filas en la tabla 
+INSERT INTO second_table (id, name, score) VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob' 14),
+(4, 'George', 8)
