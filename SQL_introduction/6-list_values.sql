1aef9b8ed3cab31d2181a417418c34902cf42848
Brahia-A <10114@holbertonstudents.com>
2025-03-12 12:26:24 -0300

    ejercico 6 y 7

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- script que enumera todas las filas de la tabla
+SELECT * FROM first_table;
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- script que inserta informacion en una tabla 
+INSERT INTO first_table (name, id) VALUES (Best School, 89);
