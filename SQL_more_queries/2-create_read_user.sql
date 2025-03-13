5fd996aab3a815f9288959dec97796e7b2c399e2
Brahia-A <10114@holbertonstudents.com>
2025-03-13 17:37:39 -0300

    ejercicio 3 crea un usuraio y una base de datos

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script que crea un usuario y una base de datos 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
