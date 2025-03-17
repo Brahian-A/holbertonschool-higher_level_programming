73f82ba6aab49c081e73e9cf94e8bb15fedc25bc
Brahia-A <10114@holbertonstudents.com>
2025-03-16 22:14:07 -0300

    15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,5 @@
 -- Script que enumera todos los programas de comedia en la base de datos SELECT tv_shows.title
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
