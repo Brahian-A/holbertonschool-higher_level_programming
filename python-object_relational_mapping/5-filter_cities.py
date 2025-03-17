d4f09dda0249fda233fc72cd124c16d43752ea95
Brahia-A <10114@holbertonstudents.com>
2025-03-16 23:58:18 -0300

    5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""toma el nombre de un estado como argumento y
+enumera todas las ciudades de ese estado,
+Usando la base de datos HBTN_0E_4_USA"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
