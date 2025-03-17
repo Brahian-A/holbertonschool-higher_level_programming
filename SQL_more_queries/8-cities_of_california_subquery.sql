82ce37b2a21666c5281d951ede685bab7b87a376
Brahia-A <10114@holbertonstudents.com>
2025-03-16 21:21:21 -0300

    8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Seleciona todas las ciudades dentro de california
 SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California');
-ORDER BY id;
+ORDER BY id ASC;
