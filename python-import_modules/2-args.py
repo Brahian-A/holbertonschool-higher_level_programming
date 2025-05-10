39fcb80b6a80c6fe552d5dbcda01ca6847701d53
Brahian-A <10114@holbertonstudents.com>
2025-05-10 01:43:07 -0300

    tasks

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args_len = len(sys.argv) - 1
+
+    if args_len == 1:
+        print("{} argument:".format(args_len))
+    elif args_len == 0:
+        print("{} arguments.".format(args_len))
+    else:
+        print("{} arguments:".format(args_len))
+
+    idx = 1
+    for arg in sys.argv[1:]:
+        print("{}: {}". format(idx, arg))
+        idx += 1
