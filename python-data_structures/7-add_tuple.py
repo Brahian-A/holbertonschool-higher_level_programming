474f4b6d9603cf5a88d2294091b1a9f63185036c
Brahia-A <10114@holbertonstudents.com>
2025-05-14 15:10:01 -0300

    lista no tupla

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -10,4 +10,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tupla_b_1 = tuple_b[0] if len(tuple_b) > 0 else 0
     tupla_b_2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
-    return [tupla_a_1 + tupla_b_1, tupla_a_2 + tupla_b_2]
+    return (tupla_a_1 + tupla_b_1, tupla_a_2 + tupla_b_2)
