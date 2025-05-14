165c03f4a8273a21838447aa26a54a3c455bf493
Brahian-A <10114@holbertonstudents.com>
2025-05-14 06:15:55 -0300

    initial commit

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """
+    Elimina el elemento en la posición dada de la lista y devuelve la lista modificada.
+    """
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+    return my_list
