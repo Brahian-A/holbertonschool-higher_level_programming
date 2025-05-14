dbb7bdc3daed8d746cacb4128104d6ab6cc5094c
Brahia-A <10114@holbertonstudents.com>
2025-05-14 09:08:28 -0300

    pycode

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     """
-    Elimina el elemento en la posición 
+    Elimina el elemento en la posición
     dada de la lista y devuelve la lista modificada.
     """
     if 0 <= idx < len(my_list):
