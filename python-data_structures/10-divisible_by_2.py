515687140ea7f417fa2279c8d77b90d039b9e196
Brahia-A <10114@holbertonstudents.com>
2025-05-14 09:05:37 -0300

    11 y 12

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    """encuentra todos los elementos de una lista y devuelve una nueva lista
+    """devuelve una nueva lista
     con True o False si el elemento es divisible por 2 o no
     """
     return [True if i % 2 == 0 else False for i in my_list]
