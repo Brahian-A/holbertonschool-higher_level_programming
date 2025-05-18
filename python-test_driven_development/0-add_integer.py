58d7ce446c56f51923747e3802bbee49b0f2a55e
Brahian-A <10114@holbertonstudents.com>
2025-05-18 20:08:09 -0300

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"funcion que suma 2 variables casteadas a enteros"
+
 
 def add_integer(a, b=98):
+    "funcion que suma variables y maneja errores"
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return( int(a) + int(b))
+
+    return (int(a) + int(b))
