b0698b10ae46ba2455fca81ce4d969f052861959
Brahian-A <10114@holbertonstudents.com>
2025-05-19 09:46:46 -0300

    0 y 1 pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Modulo que crea una clase"""
+
+
 class Square:
     """creamos una clase square vacia"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""En este modulo creamos una clase con el atributo privado size"""
+
 
 class Square():
+    """Definicion de clase con un atributo privado"""
     def __init__(self, size):
         self.__size = size
-        
