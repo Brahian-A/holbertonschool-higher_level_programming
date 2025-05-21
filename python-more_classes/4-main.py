b0e4171777c3eb939eaccecb1e2ebde9a853e889
Brahia-A <10114@holbertonstudents.com>
2025-05-21 12:29:41 -0300

    task 4

--- python-more_classes/4-main.py
+++ python-more_classes/4-main.py
@@ -24,3 +24,7 @@ print("--")
 
 print(new_rectangle is my_rectangle)
 print(type(new_rectangle) is type(my_rectangle))
+
+myrectangle = Rectangle(2, 4) 
+print(str(myrectangle)) 
+print(repr(my_rectangle))
