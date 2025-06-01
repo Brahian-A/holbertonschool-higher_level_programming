0552a12794d1b934eeca0e8ab938a9497fb7d745
Brahian-A <10114@holbertonstudents.com>
2025-05-31 22:32:38 -0300

    pycode

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Modulo que contiene la funcion is_same_class"""
 
+
 def is_same_class(obj, a_class):
     """retorna True si obj es una instancia de a_class"""
     return type(obj) is a_class
