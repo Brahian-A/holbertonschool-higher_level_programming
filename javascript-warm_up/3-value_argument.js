c2d2fddc3dc926ac799d3d48556f4f862037bb45
Brahian-A <10114@holbertonstudents.com>
2025-07-13 23:44:40 -0300

    ahora si

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-console.log(`${argv[2]} is ${argv[3]}`);
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
