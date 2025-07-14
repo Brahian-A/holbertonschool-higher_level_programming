ff797546d66965ed02a575391f232a8c9aabc8d0
Brahian-A <10114@holbertonstudents.com>
2025-07-13 23:34:27 -0300

    ejercicio 2 argumentos

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2); // nos saltamos los dos primeros porque el primero tiene la ruta para ejercutar node y el segundo tiene la ruta al archivo js
+
+if (args.length === 0) {
+  console.log("No argument");
+} 
+else if (args.length === 1) {
+  console.log("Argument found");
+} 
+else {
+  console.log("Arguments found");
+}
